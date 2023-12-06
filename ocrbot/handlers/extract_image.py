e5c414774c08f2145bfc74587b1474d7965076e1
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-12-06 11:03:56 +0530

    Update extract_image.py

--- ocrbot/handlers/extract_image.py
+++ ocrbot/handlers/extract_image.py
@@ -15,13 +15,13 @@ def extract_image(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     file_id = update.message.photo[-1].file_id
     file_path = context.bot.get_file(file_id).file_path
-    m = update.message.reply_text('Extracting...', quote=True)
+    m = update.message.reply_text('...', quote=True)
     if file_path is not None:
         data = requests.get(f"https://api.ocr.space/parse/imageurl?apikey={API_KEY}&url={file_path}&language=chs&detectOrientation=True&filetype=JPG&OCREngine=1&isTable=True&scale=True").json()
         if data['IsErroredOnProcessing'] == False:
             message = data['ParsedResults'][0]['ParsedText']
-            query.edit_message_text(text=message)
+            m.edit_text(text=message)
         else:
-            query.edit_message_text(text="⚠️Something went wrong, please try again later ⚠️")
+            m.edit_text(text="⚠️Something went wrong, please try again later ⚠️")
     else:
-        query.edit_message_text("Something went wrong, Send this image again")
+        m.edit_text("Something went wrong, Send this image again")
